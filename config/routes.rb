13d96cb0a0c1a067ae41461c68a0298c44b018f8
Chia-Tse Weng <wengyagami@hotmail.com>
2019-08-09 12:11:55 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/cocktails/update_list', to: 'cocktails#update_list'
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_065318) do
+ActiveRecord::Schema.define(version: 2019_08_09_022935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_08_065318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.string "pic"
   end
 
   create_table "doses", force: :cascade do |t|
